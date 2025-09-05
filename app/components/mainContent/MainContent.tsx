1b21cfb84fab7a4e131093404b3dd5d4cb641504
dachi <ani10sanadze@gmail.com>
2025-09-05 09:09:28 +0400

    make base outline, ready for start

---
+++ app/components/mainContent/MainContent.tsx
@@ -0,0 +1,13 @@
+"use client"
+import styles from "./MainContent.module.css";
+
+
+const MainContent = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default MainContent;
---
+++ app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+"use client"
+import styles from "./Sidebar.module.css";
+
+
+const Sidebar = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+
+export default Sidebar;
